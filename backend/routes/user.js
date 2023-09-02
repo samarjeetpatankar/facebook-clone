e764a162c22cdc445cbb63036e1bebf453aaa794
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-02 19:52:35 +0530

    resend verification mail

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,13 +4,16 @@ const {
   activateAccount,
   login,
   auth,
+  sendVerification,
 } = require("../controllers/user");
 const { authUser } = require("../middlwares/auth");
 
 const router = express.Router();
-
 router.post("/register", register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login", login);
+router.post("/sendVerification",authUser, sendVerification);
 
 module.exports = router;
+
+
