7df53fefd2f8700be7b68f56fe919b01c55f5d54
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-02 17:14:22 +0530

    right side

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
+import RightHome from "../../components/home/right";
 
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Header />
       <LeftHome user={user} />
+      <RightHome user={user} />
     </div>
   );
 }
