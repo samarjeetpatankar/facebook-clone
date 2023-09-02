d256bfb0258afeb01f5ea0eafedff18f1aa89584
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-02 17:28:54 +0530

    post form

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
@@ -13,6 +14,7 @@ export default function Home() {
       <LeftHome user={user} />
       <div className="home_middle">
         <Stories />
+        <CreatePost user={user} />
       </div>
       <RightHome user={user} />
     </div>
