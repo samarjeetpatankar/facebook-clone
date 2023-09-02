febab2ab89543b46a768eb99f5ff3db9980cfced
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-02 16:57:43 +0530

    left home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
-import LoggedInRoutes from "./routes/LoggedInRoutes";
-import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 function App() {
   return (
     <div>
       <Routes>
-        <Route element={<LoggedInRoutes />}>
-          <Route path="/profile" element={<Profile />} exact />
-          <Route path="/" element={<Home />} exact />
-        </Route>
-        <Route element={<NotLoggedInRoutes />}>
-          <Route path="/login" element={<Login />} exact />
-        </Route>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} exact />
       </Routes>
     </div>
   );
