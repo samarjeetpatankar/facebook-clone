43f7afc279d3608ddd98bf9abbf27733fb88bce7
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-06 15:51:40 +0530

    profile header and cover

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { profileReducer } from "../../functions/reducers";
-
+import Header from "../../components/header";
+import "./style.css";
+import Cover from "./Cover";
 export default function Profile() {
   const { username } = useParams();
   const navigate = useNavigate();
@@ -45,6 +47,14 @@ export default function Profile() {
       });
     }
   };
-  console.log(profile);
-  return <div>Profile</div>;
+  return (
+    <div className="profile">
+      <Header page="profile" />
+      <div className="profile_top">
+        <div className="profile_container">
+          <Cover cover={profile.cover} />
+        </div>
+      </div>
+    </div>
+  );
 }
