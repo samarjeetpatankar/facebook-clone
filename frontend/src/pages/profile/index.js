209239fd3d8fe75c267fc34a7ff60bfd422622a2
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-06 16:28:31 +0530

    display profile post

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -11,11 +11,13 @@ import ProfileMenu from "./ProfileMenu";
 import PplYouMayKnow from "./PplYouMayKnow";
 import CreatePost from "../../components/createPost";
 import GridPosts from "./GridPosts";
+import Post from "../../components/post";
 export default function Profile({ setVisible }) {
   const { username } = useParams();
   const navigate = useNavigate();
   const { user } = useSelector((state) => ({ ...state }));
   var userName = username === undefined ? user.username : username;
+
   const [{ loading, error, profile }, dispatch] = useReducer(profileReducer, {
     loading: false,
     profile: {},
@@ -24,6 +26,9 @@ export default function Profile({ setVisible }) {
   useEffect(() => {
     getProfile();
   }, [userName]);
+  var visitor = userName === user.username ? false : true;
+  console.log(visitor);
+
   const getProfile = async () => {
     try {
       dispatch({
@@ -57,8 +62,8 @@ export default function Profile({ setVisible }) {
       <Header page="profile" />
       <div className="profile_top">
         <div className="profile_container">
-          <Cover cover={profile.cover} />
-          <ProfielPictureInfos profile={profile} />
+          <Cover cover={profile.cover} visitor={visitor} />
+          <ProfielPictureInfos profile={profile} visitor={visitor} />
           <ProfileMenu />
         </div>
       </div>
@@ -69,8 +74,19 @@ export default function Profile({ setVisible }) {
             <div className="profile_grid">
               <div className="profile_left"></div>
               <div className="profile_right">
-                <CreatePost user={user} profile setVisible={setVisible} />
+                {!visitor && (
+                  <CreatePost user={user} profile setVisible={setVisible} />
+                )}
                 <GridPosts />
+                <div className="posts">
+                  {profile.posts && profile.posts.length ? (
+                    profile.posts.map((post) => (
+                      <Post post={post} user={user} key={post._id} />
+                    ))
+                  ) : (
+                    <div className="no_posts">No posts available</div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
