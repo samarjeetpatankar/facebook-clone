e92fd6a7a0928458dcef5cb66295055b1d78f9c0
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-07 10:30:18 +0530

    fixing errors

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -74,7 +74,7 @@ exports.register = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
-    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    const url = `/activate/${emailVerificationToken}`; 
     sendVerificationEmail(user.email, user.first_name, url);
     const token = generateToken({ id: user._id.toString() }, "7d");
     res.send({
@@ -160,7 +160,7 @@ exports.sendVerification = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
-    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    const url = `/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
     return res.status(200).json({
       message: "Email verification link has been sent to your email.",
@@ -528,3 +528,4 @@ exports.deleteRequest = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
 //database
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(process.env.DATABASE_URL, { 
     useNewUrlParser: true,
   })
   .then(() => console.log("database connected successfully"))
@@ -29,3 +29,5 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}..`);
 });
+ 
+
