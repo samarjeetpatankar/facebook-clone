491e53771f44dea7f294bb147ace8bf222600577
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-07 11:44:04 +0530

    save unsave download and delete post

--- frontend/src/functions/post.js
+++ frontend/src/functions/post.js
@@ -84,3 +84,36 @@ export const comment = async (postId, comment, image, token) => {
     return error.response.data.message;
   }
 };
+export const savePost = async (postId, token) => {
+  try {
+    const { data } = await axios.put(
+      `${process.env.REACT_APP_BACKEND_URL}/savePost/${postId}`,
+      {},
+
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+export const deletePost = async (postId, token) => {
+  try {
+    const { data } = await axios.delete(
+      `${process.env.REACT_APP_BACKEND_URL}/deletePost/${postId}`,
+
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
