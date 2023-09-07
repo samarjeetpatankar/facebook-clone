8d8d5b1716a538421520a6ba48cd060a7da976b0
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-07 17:08:02 +0530

    fixing errors

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -172,6 +172,7 @@ export default function Profile({ getAllPosts }) {
                       {Array.from(new Array(6), (val, i) => i + 1).map(
                         (id, i) => (
                           <Skeleton
+                            key={`skeleton-${i}`}
                             circle
                             height="32px"
                             width="32px"
