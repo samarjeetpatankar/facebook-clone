00db81a218c1c99a1def1661e7054133eea8fc19
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-11 14:39:49 +0530

    update

--- frontend/src/components/home/right/Contact.js
+++ frontend/src/components/home/right/Contact.js
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from "react";
-
 export default function Contact({ user }) {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/getProfile`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setUsers(data);
-        } else {
-          console.error("Data is not an array: ", data);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching users: ", error);
-      });
-  }, []);
-
   return (
-    <div>
-      {users.map((user) => (
-        <div className="contact hover3" key={user._id}>
-          <div className="contact_img">
-            <img src={user.picture} alt="" />
-          </div>
-          <span>
-            {user.first_name} {user.last_name}
-          </span>
-        </div>
-      ))}
+    <div className="contact hover3">
+      <div className="contact_img">
+        <img src={user.picture} alt="" />
+      </div>
+      <span>
+        {user.first_name} {user.last_name}
+      </span>
     </div>
   );
 }
+ 
+
