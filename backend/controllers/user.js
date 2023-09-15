67575ebb0aad22269c67437b685ae52c271baf6e
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-15 18:45:06 +0530

    email error

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -87,7 +87,7 @@ exports.register = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
-    const url = `/activate/${emailVerificationToken}`;
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
     const token = generateToken({ id: user._id.toString() }, "7d");
     res.send({
@@ -176,7 +176,7 @@ exports.sendVerification = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
-    const url = `/activate/${emailVerificationToken}`;
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
     return res.status(200).json({
       message: "Email verification link has been sent to your email.",
