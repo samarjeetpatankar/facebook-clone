6cbe93565c3f3dbb6422b212f59432fdc949fd77
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-09-25 13:10:55 +0530

    changes

--- frontend/src/components/post/index.js
+++ frontend/src/components/post/index.js
@@ -89,11 +89,11 @@ export default function Post({ post, user, profile }) {
               <div className="updated_p">
                 {post.type === "profilePicture" &&
                   `updated ${
-                    post.user.gender === "male" ? "her" : "his"
+                    post.user.gender === "male" ? "his" : "her"
                   } profile picture`}
                 {post.type === "coverPicture" &&
                   `updated ${
-                    post.user.gender === "male" ? "her" : "his"
+                    post.user.gender === "male" ? "his" : "her"
                   } cover picture`}
               </div>
             </div>
