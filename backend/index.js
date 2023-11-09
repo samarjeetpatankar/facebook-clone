df7939003f0421c1b54c5d2512ac9501bf0d99d2
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-11-09 19:56:21 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
@@ -10,12 +9,7 @@ mongoose.set('strictQuery', false);
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-  origin: 'http://localhost:3000', 
-  optionsSuccessStatus: 200, 
-}; 
 
-app.use(cors(corsOptions));
 app.use( 
   fileUpload({
     useTempFiles: true,
